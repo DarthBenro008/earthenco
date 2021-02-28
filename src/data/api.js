7ef2cd489d261a558b4754c83d5a2d042ab6edb4
DarthBenro008 <hkpdev008@gmail.com>
2021-02-28 10:01:59 +0530

    fixes: minor changes

--- src/data/api.js
+++ src/data/api.js
@@ -3,12 +3,8 @@ const axios = require("axios");
 const fetchWeather = async (lat, lon) => {
   try {
     const apiKey = process.env.REACT_APP_KEY_OWA;
-    // const response = await axios.get(
-    //   `http://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${apiKey}`
-    // );
-    
     const response = await axios.get(
-      `https://jsonplaceholder.typicode.com/todos/1`
+      `http://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${apiKey}`
     );
     console.log("damn", response.data);
     return response.data;
--- src/data/data.js
+++ src/data/data.js
@@ -1,10 +1,10 @@
 export const trends = [
   {
     id: "marker1",
-    city: "Singapore",
+    city: "Pulau Ujong",
     color: "red",
-    countryCode: "IN",
-    countryName: "IN",
+    countryCode: "SGP",
+    countryName: "Singapore",
     coordinates: [1.3521, 103.8198],
     value: 50,
   },
@@ -14,8 +14,8 @@ export const trends = [
     color: "blue",
     coordinates: [40.73061, -73.935242],
     value: 25,
-    countryCode: "IN",
-    countryName: "IN",
+    countryCode: "USA",
+    countryName: "United States",
   },
   {
     id: "marker3",
@@ -23,8 +23,8 @@ export const trends = [
     color: "orange",
     coordinates: [37.773972, -122.431297],
     value: 35,
-    countryCode: "IN",
-    countryName: "IN",
+    countryCode: "USA",
+    countryName: "United States",
   },
   {
     id: "marker4",
@@ -32,8 +32,8 @@ export const trends = [
     color: "gold",
     coordinates: [39.9042, 116.4074],
     value: 135,
-    countryCode: "IN",
-    countryName: "IN",
+    countryCode: "CN",
+    countryName: "China",
   },
   {
     id: "marker5",
@@ -41,8 +41,8 @@ export const trends = [
     color: "green",
     coordinates: [51.5074, 0.1278],
     value: 80,
-    countryCode: "IN",
-    countryName: "IN",
+    countryCode: "EU",
+    countryName: "England",
   },
   {
     id: "marker6",
@@ -50,8 +50,17 @@ export const trends = [
     color: "gold",
     coordinates: [29.7604, -95.3698],
     value: 54,
-    countryCode: "IN",
-    countryName: "IN",
+    countryCode: "USA",
+    countryName: "United States",
+  },
+  {
+    id: "marker6",
+    city: "Los Angeles",
+    color: "gold",
+    coordinates: [29.7604, -95.3698],
+    value: 54,
+    countryCode: "USA",
+    countryName: "United States",
   },
 ];
 
